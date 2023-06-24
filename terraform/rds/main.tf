b43151fab8f03890986b94342e042b4e4f301a87
Abdulaziz <121072628+Abdulaziz-19@users.noreply.github.com>
2023-06-24 16:39:30 -0500

    added ami

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -2,3 +2,16 @@ resource "aws_db_subnet_group" "ci-cd" {
   name       = "ci-cd-demo"
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
+
+/*resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.ci-cd.name
+*/}
