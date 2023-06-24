c7faeeafd90333885d788a16350840b0d13a3cad
Abdulaziz <121072628+Abdulaziz-19@users.noreply.github.com>
2023-06-24 16:43:33 -0500

    removed comment

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_subnet_group" "ci-cd" {
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
 
-/*resource "aws_db_instance" "default" {
+resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
@@ -14,4 +14,4 @@ resource "aws_db_subnet_group" "ci-cd" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.ci-cd.name
-*/}
+}
